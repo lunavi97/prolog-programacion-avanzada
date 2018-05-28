21c5b240b47139c9175b5d498c8731e1efeaff2b
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-05-28 20:21:02 -0300

    Initial clauses of the menu

---
+++ menu.pl
@@ -0,0 +1,17 @@
+%clauses
+
+entrada(paella).
+entrada(gazpacho).
+entrada(consomé).
+
+carne(filete_de_cerdo).
+carne(pollo_asado).
+
+pescado(trucha).
+pescado(bacalao).
+
+postre(flan).
+postre(helado).
+postre(pastel).
+
+%fin clauses
