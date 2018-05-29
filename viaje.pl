7cf631561238a4db11dd5c23e4c7aa7a1468d664
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-05-28 21:31:36 -0300

    Declaraciones iniciales de transporte y alojamiento

---
+++ viaje.pl
@@ -0,0 +1,15 @@
+%clauses
+transporte(roma,20).
+transporte(londres,30).
+transporte(tunez,10).
+
+alojamiento(roma,hotel,50).
+alojamiento(roma,hostal,30).
+alojamiento(roma,camping,10).
+alojamiento(londres,hotel,60).
+alojamiento(londres,hostal,40).
+alojamiento(londres,camping,20).
+alojamiento(tunez,hotel,40).
+alojamiento(tunez,hostal,20).
+alojamiento(tunez,camping,5).
+%fin clauses
