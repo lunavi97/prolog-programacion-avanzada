d49474ee532ff4ceb39429e08dba04c2d8e9dc78
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-05-28 21:28:24 -0300

    Fix #1 Correción de hermandad

--- arbol-genealogico.pl
+++ arbol-genealogico.pl
@@ -13,6 +13,6 @@ padre(pedro, maría).
 niño(X, Y) :- padre(Y, X). %Expresa que X es hijo o hija de Y.
 hijo(X, Y) :- niño(X, Y), hombre(X). %Expresa que X es un hijo varón de Y.
 hija(X, Y) :- niño(X, Y), mujer(X). %Expresa que X es una hija de Y.
-hermano_o_hermana(X, Y) :- niño(X, _), niño(Y, _). %Expresa que X es hermano o hermana de Y.
+hermano_o_hermana(X, Y) :- niño(X, _), niño(Y, _), not(X == Y). %Expresa que X es hermano o hermana de Y.
 hermano(X, Y) :- hermano_o_hermana(X, Y), hombre(X). %Expresa que X es un hermano de Y.
 hermana(X, Y) :- hermano_o_hermana(X, Y), mujer(X). %Expresa que X es una hermana de Y.
