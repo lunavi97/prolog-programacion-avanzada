f1844cba8874dd2a81bcd265a684bde13fe6eb35
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-05-29 03:55:30 -0300

    Producto aplicando sumas sucesivas

--- recursividad.pl
+++ recursividad.pl
@@ -18,3 +18,19 @@ factorial(N, Y) :-
     N1 is N - 1,
     factorial(N1, Y1),
     Y is N * Y1.
+
+producto_sumas_sucesivas(0, _, 0) :- !.
+producto_sumas_sucesivas(_, 0, 0) :- !.
+producto_sumas_sucesivas(X, Y, P) :-
+    Y > 0,
+    Y1 is Y - 1,
+    producto_sumas_sucesivas(X, Y1, P1),
+    P is X + P1, !.
+producto_sumas_sucesivas(X, Y, P) :-
+    X > 0,
+    X1 is X - 1,
+    producto_sumas_sucesivas(X1, Y, P1),
+    P is Y + P1, !.
+producto_sumas_sucesivas(X, Y, P) :-
+    XP is X * -1, YP is Y * -1,
+    producto_sumas_sucesivas(XP, YP, P).
